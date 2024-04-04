941b1833d8aaa45f7c74c5c02ee8100387869ed8
davexpert <mrdavexpertkg@gmail.com>
2024-04-04 10:46:13 +0600

    media + static

--- agroskill/urls.py
+++ agroskill/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from agrosite.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_page_view)
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
